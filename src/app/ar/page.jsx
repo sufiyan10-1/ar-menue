88a3460e0c1a2e7a5bc9340cbeb1b50093fd154e
sufiyan10-1 <134135723+sufiyan10-1@users.noreply.github.com>
2025-07-05 08:48:31 +0530

    view image in ar

--- src/app/ar/page.jsx
+++ src/app/ar/page.jsx
@@ -1,41 +1,9 @@
-'use client'
-
-import Head from "next/head";
-
+import dynamic from "next/dynamic";
+ 
+ 
 export default function ARPage() {
   return (
     <>
-      <Head>
-        {/* A-Frame and MindAR CDN */}
-        <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/mind-ar@1.1.4/dist/mindar-image-aframe.prod.js"></script>
-      </Head>
-
-      <div>
-        <a-scene
-          mindar-image="imageTargetSrc: /targets.mind; autoStart: true;"
-          device-orientation-permission-ui="enabled: true"
-          loading-screen="enabled: true"
-          embedded
-          vr-mode-ui="enabled: false"
-          renderer="colorManagement: true; physicallyCorrectLights: true"
-        >
-          <a-assets>
-            <img id="overlay-image" src="/overlay.png" />
-          </a-assets>
-
-          <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-
-          <a-entity mindar-image-target="targetIndex: 0">
-            <a-image
-              src="#overlay-image"
-              position="0 0 0"
-              width="1"
-              height="1"
-            ></a-image>
-          </a-entity>
-        </a-scene>
-      </div>
     </>
   );
 }
