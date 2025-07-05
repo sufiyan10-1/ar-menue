9b294e87aae3e0cdb36d519f70311e9b4f2521a2
sufiyan10-1 <134135723+sufiyan10-1@users.noreply.github.com>
2025-07-05 09:35:14 +0530

    rotated problem

--- src/app/ar/page.jsx
+++ src/app/ar/page.jsx
@@ -1,9 +1,18 @@
-import dynamic from "next/dynamic";
- 
- 
+'use client';
+
 export default function ARPage() {
   return (
-    <>
-    </>
+    <iframe
+      src="/ar-view.html"
+      style={{
+        width: '90vw',
+        height: '90vh',
+        border: 'none',
+        overflow: 'hidden',
+       
+      }}
+      allow="camera"
+      title="AR View"
+    />
   );
 }
